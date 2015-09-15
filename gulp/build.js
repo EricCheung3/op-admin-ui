e2168068a3ef89ef5c36909495ac4f1cd1925034
Yuan Ji <jiwhiz@gmail.com>
2015-09-15 08:40:22 -0600

    fix build

--- gulp/build.js
+++ gulp/build.js
@@ -23,7 +23,7 @@ gulp.task('partials', function () {
       }))
     )
     .pipe($.angularTemplatecache('templateCacheHtml.js', {
-      module: 'app',
+      module: 'openprice.admin',
       root: 'app'
     }))
     .pipe(gulp.dest(paths.tmp + '/partials/'));
